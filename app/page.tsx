bcca9f0b2e89887e7dcb9eeff576ec9e833c211e
akhan <akhan.dulatbay@gmail.com>
2025-01-19 12:24:33 +0500

    fix: add header to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import LearnPage from "@/app/(main-pages)/learn/page";
+import Header from "@/components/Header/header";
 
 export default function Home() {
     return (
-        <LearnPage/>
+        <>
+            <Header/>
+            <LearnPage/>
+        </>
     );
 }
