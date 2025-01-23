f0381562b698caa648e34790132ec1aceb3e7e4c
akhan <akhan.dulatbay@gmail.com>
2025-01-23 09:23:07 +0500

    fix: api url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API_URL: 'http://185.32.84.190/api'
+    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
