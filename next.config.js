9353748c9e762bb44b4e066c3c6d0cdc1a5523cd
akhan <akhan.dulatbay@gmail.com>
2025-01-27 16:34:27 +0500

    fix: url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_API_URL: 'http://localhost:8080/api'
+        NEXT_PUBLIC_API_URL: 'http://185.32.84.190/api'
     },
     eslint: {
 
